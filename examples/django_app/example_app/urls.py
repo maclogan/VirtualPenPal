53d419a5950c959ff0edcaf5e6c5a9470d9c5d27
Mac <p13mlogan@hotmail.com>
2017-03-05 18:43:35 -0600

    User registration added.  Need to add authentification.

--- examples/django_app/example_app/urls.py
+++ examples/django_app/example_app/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
 from example_app.views import ChatterBotAppView
+from . import views
 
 
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
+    url(r'^register/', views.UserFormView.as_view(), name='register'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
 ]
-
--- examples/django_app/example_app/views.py
+++ examples/django_app/example_app/views.py
@@ -1,6 +1,35 @@
 from django.views.generic.base import TemplateView
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
+from .forms import UserForm
 
 class ChatterBotAppView(TemplateView):
     template_name = "app.html"
 
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'user_registration.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = autheticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('')
+
+        return render(request, self.template_name, {'form':form})
